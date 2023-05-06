d0aca0b46faaf5df44117fd44acd2ca7abf6bcf1
Enzo0721 <38281095+Enzo0721@users.noreply.github.com>
2023-05-06 19:08:48 -0400

    Update settings.h

--- settings.h
+++ settings.h
@@ -11,9 +11,13 @@ namespace settings {
 	inline bool toggle_noFlash = false;
 	inline bool toggle_pulseGlow = false;
 	inline bool toggle_aimbot = false;
-	inline int chamsColor = 1;
+	inline float chamsColor[] = { 1.f, 0.f, 0.f, 1.f };
 	inline float glowColor[] = { 1.f, 0.f, 0.f, 1.f };
 	inline int aimbotFOV = 0;
+	inline int glowType = 0;
+	inline bool toggle_healthGlow = false;
+	inline bool toggle_teamGlow = false;
+	inline float teamGlowColor[] = { 1.f, 0.f, 0.f, 1.f };
 }
 
 # define ReadNextSetting(s) myconfig.ignore(80, '='); myconfig >> s
