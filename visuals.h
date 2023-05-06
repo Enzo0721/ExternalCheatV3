b8c3880ffb0aa2bb92fc1bc7104581f0ce711801
Enzo0721 <38281095+Enzo0721@users.noreply.github.com>
2023-05-06 19:08:09 -0400

    Update visuals.h

--- visuals.h
+++ visuals.h
@@ -4,6 +4,7 @@
 namespace visuals
 {
 	void visualThread(const Memory& mem) noexcept;
-	void makeGlow(const Memory& mem, int human);
+	void chamsThread(const Memory& mem) noexcept;
 	void glow(const Memory& mem, const uintptr_t localBase);
+	void chams(const Memory& mem, const uintptr_t localBase);
 }
