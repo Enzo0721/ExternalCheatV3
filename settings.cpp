3aebab7e604f95bb5d106cc862e01e68fbc86187
Enzo0721 <38281095+Enzo0721@users.noreply.github.com>
2023-05-06 19:08:28 -0400

    Update settings.cpp

--- settings.cpp
+++ settings.cpp
@@ -19,6 +19,13 @@ void saveConfig()
 	myconfig << "glowColor[1] = " << settings::glowColor[1] << "\n";
 	myconfig << "glowColor[2] = " << settings::glowColor[2] << "\n";
 	myconfig << "glowColor[3] = " << settings::glowColor[3] << "\n";
+	myconfig << "glowType = " << settings::glowType << "\n";
+	myconfig << "healthGlow = " << settings::toggle_healthGlow << "\n";
+	myconfig << "teamGlow = " << settings::toggle_teamGlow << "\n";
+	myconfig << "teamGlowColor[0] = " << settings::teamGlowColor[0] << "\n";
+	myconfig << "teamGlowColor[1] = " << settings::teamGlowColor[1] << "\n";
+	myconfig << "teamGlowColor[2] = " << settings::teamGlowColor[2] << "\n";
+	myconfig << "teamGlowColor[3] = " << settings::teamGlowColor[3] << "\n";
 	myconfig.close();
 }
 
@@ -33,10 +40,17 @@ void loadConfig()
 	ReadNextSetting(settings::toggle_pulseGlow);
 	ReadNextSetting(settings::toggle_aimbot);
 	ReadNextSetting(settings::aimbotFOV);
-	ReadNextSetting(settings::chamsColor);
+	//ReadNextSetting(settings::chamsColor);
 	ReadNextSetting(settings::glowColor[0]);
 	ReadNextSetting(settings::glowColor[1]);
 	ReadNextSetting(settings::glowColor[2]);
-	ReadNextSetting(settings::glowColor[0]);
+	ReadNextSetting(settings::glowColor[3]);
+	ReadNextSetting(settings::glowType);
+	ReadNextSetting(settings::toggle_healthGlow);
+	ReadNextSetting(settings::toggle_teamGlow);
+	ReadNextSetting(settings::teamGlowColor[0]);
+	ReadNextSetting(settings::teamGlowColor[1]);
+	ReadNextSetting(settings::teamGlowColor[2]);
+	ReadNextSetting(settings::teamGlowColor[3]);
 	myconfig.close();
 }
