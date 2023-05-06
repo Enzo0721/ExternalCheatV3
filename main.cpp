461033fdfa76e7684bc9d031df778c19f3a9419f
Enzo0721 <38281095+Enzo0721@users.noreply.github.com>
2023-05-06 19:10:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,27 @@
 #include <thread>
 #include "visuals.h"
 #include "aimbot.h"
+#include <iostream>
+#include <cstddef> 
+#include <cstdint>
+
 
 int __stdcall wWinMain(
 	HINSTANCE instance,
 	HINSTANCE previousInstance,
 	PWSTR arguments,
 	int commandShow)
-{
+{	
+	BOOL WINAPI AllocConsole();
+	AllocConsole();
+	FILE* fDummy;
+	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+	std::cout << "successful opening" << std::endl;
+	Sleep(1000);
+	fclose(fDummy);
+	BOOL WINAPI FreeConsole();
+	FreeConsole();
+
 	Memory mem{ "csgo.exe" };
 	globals::bClient = mem.GetModuleAddress("client.dll");
 	globals::bEngine = mem.GetModuleAddress("engine.dll");
@@ -19,6 +33,7 @@ int __stdcall wWinMain(
 	std::thread(visuals::visualThread, mem).detach();
 	std::thread(misc::miscThread, mem).detach();
 	std::thread(aimbot::aimbotThread, mem).detach();
+	std::thread(visuals::chamsThread, mem).detach();
 
 	gui::CreateHWindow("Cheat Menu", "Cheat Menu Class");
 	gui::CreateDevice();
@@ -43,3 +58,4 @@ int __stdcall wWinMain(
 int main()
 {
 }
+
